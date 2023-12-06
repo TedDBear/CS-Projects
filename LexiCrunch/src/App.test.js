36d037515c4010bc487914b5b1cded26971f84c1
Stephen Craig <130504992+TedDBear@users.noreply.github.com>
2023-12-06 15:22:05 -0600

    Delete LexiCrunch directory

--- LexiCrunch/src/App.test.js
+++
@@ -1,8 +0,0 @@
-//git add .import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App></App>);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
